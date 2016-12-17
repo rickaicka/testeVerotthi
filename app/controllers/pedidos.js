759bab188b870f53126d50dbaf35d01b60880d62
Ricardo Salim <ricardosalim7@hotmail.com>
2016-12-16 22:51:59 -0200

    Tela de pedidos

--- app/controllers/pedidos.js
+++ app/controllers/pedidos.js
@@ -1,6 +1,6 @@
 var pedidos =  [
             {_id: 1, nome:"Smartphone", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus ut elit at pulvinar. Morbi ac posuere felis. In id ornare erat. Praesent lacinia quis risus id rutrum.", imagem:"smartphone.jpg"},
-            {_id: 2, nome:"Playstation 4", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus ut elit at pulvinar. Morbi ac posuere felis. In id ornare erat. Praesent lacinia quis risus id rutrum.", imagem:"ps4.jpg"},
+            {_id: 2, nome:"Console Playstation 4 500GB", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus ut elit at pulvinar. Morbi ac posuere felis. In id ornare erat. Praesent lacinia quis risus id rutrum.", imagem:"ps4.jpg"},
             {_id: 3, nome:"Câmera", descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus ut elit at pulvinar. Morbi ac posuere felis. In id ornare erat. Praesent lacinia quis risus id rutrum.", imagem:"camera.jpg"},
         ];
 
